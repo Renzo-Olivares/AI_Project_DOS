5bfb044c44b45ed54570c60ec30a39379b7308c9
Renzo <rmolivares@renzo-olivares.dev>
2021-03-17 18:29:55 -0700

    make leave_one_out_cross_validation universal

--- feature_search.py
+++ feature_search.py
@@ -32,7 +32,7 @@ def forward_search(data):
                 trace.write(f'\t--Considering adding feature {k}\n')
                 trace.close()
 
-                accuracy = leave_one_out_cross_validation_forward(data, list(current_set_of_features.values()), k)
+                accuracy = leave_one_out_cross_validation(data, list(current_set_of_features.values()), None, k)
 
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
@@ -92,7 +92,7 @@ def backward_search(data):
                 trace.write(f'\t--Considering removing feature {k}\n')
                 trace.close()
 
-                accuracy = leave_one_out_cross_validation_backwards(data, list(current_set_of_features.values()), set_of_features_removed, k)
+                accuracy = leave_one_out_cross_validation(data, list(current_set_of_features.values()), set_of_features_removed, k)
 
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
@@ -125,16 +125,21 @@ def backward_search(data):
     trace = open('trace.txt', 'a')
     trace.write(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {"{:.2f}".format(max(all_set_of_features)[0])}%\n')
     trace.close() 
-# Exclude anything not in current feature set and exclude featureToAdd
 
-def leave_one_out_cross_validation_backwards(data, currentFeatureSet, featuresToExclude, featureToRemove):
+def leave_one_out_cross_validation(data,currentFeatureSet, featuresToExclude, featureToAddOrRemove):
     number_correct_classified = 0
     temp_data = copy.deepcopy(data)
 
-    for i in range(len(temp_data)): # Exclude anything not in our excluded features or our feature being removed
-        for j in range(len(temp_data[0])):
-            if (j in featuresToExclude or j is featureToRemove) and j != 0:
-                temp_data[i][j] = 0
+    if featuresToExclude is None:
+        for i in range(len(temp_data)): # Exclude anything not in the current feature set of the feature we are adding
+            for j in range(len(temp_data[0])):
+                if (j not in currentFeatureSet and j is not featureToAddOrRemove) and j != 0:
+                    temp_data[i][j] = 0
+    else:
+        for i in range(len(temp_data)): # Exclude anything not in our excluded features or our feature being removed
+            for j in range(len(temp_data[0])):
+                if (j in featuresToExclude or j is featureToAddOrRemove) and j != 0:
+                    temp_data[i][j] = 0
 
     for i in range(len(temp_data)):
         object_to_classify = temp_data[i][1:len(temp_data[i])]
@@ -143,12 +148,8 @@ def leave_one_out_cross_validation_backwards(data, currentFeatureSet, featuresTo
         nearest_neighbor_distance = float('inf')
         nearest_neighbor_location = float('inf')
 
-        # print(f'Looping over i at the {i + 1} location')
-        # print(f'The {i + 1}th object is in class {label_object_to_classify}')
-
         for k in range(len(temp_data)):
             if k != i: # Don't compare to yourself!!!!
-                # print(f'Ask if {i+1} is nearest neighbor with {k + 1}')
                 distance = distance_calculator(object_to_classify, temp_data[k][1:len(temp_data[k])])
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
@@ -158,10 +159,12 @@ def leave_one_out_cross_validation_backwards(data, currentFeatureSet, featuresTo
         if label_object_to_classify == nearest_neighbor_label:
             number_correct_classified = number_correct_classified + 1
 
-        #print(f'The {i + 1}th object is in class {label_object_to_classify}')
-        #print(f'Its nearest_neighbor is {nearest_neighbor_location} which is in class {nearest_neighbor_label}')
     temp_features = currentFeatureSet[:]
-    temp_features.remove(featureToRemove)
+
+    if featuresToExclude is None:
+        temp_features.append(featureToAddOrRemove)
+    else:
+        temp_features.remove(featureToAddOrRemove)
 
     accuracy = (number_correct_classified / len(temp_data)) * 100
 
@@ -172,53 +175,6 @@ def leave_one_out_cross_validation_backwards(data, currentFeatureSet, featuresTo
     trace.close()
     
     return accuracy
-
-def leave_one_out_cross_validation_forward(data, currentFeatureSet, featureToAdd):
-    number_correct_classified = 0
-    temp_data = copy.deepcopy(data)
-
-    for i in range(len(temp_data)): # Exclude anything not in the current feature set of the feature we are adding
-        for j in range(len(temp_data[0])):
-            if (j not in currentFeatureSet and j is not featureToAdd) and j != 0:
-                temp_data[i][j] = 0
-
-    for i in range(len(temp_data)):
-        object_to_classify = temp_data[i][1:len(temp_data[i])]
-        label_object_to_classify = temp_data[i][0]
-
-        nearest_neighbor_distance = float('inf')
-        nearest_neighbor_location = float('inf')
-
-        # print(f'Looping over i at the {i + 1} location')
-        # print(f'The {i + 1}th object is in class {label_object_to_classify}')
-
-        for k in range(len(temp_data)):
-            if k != i: # Don't compare to yourself!!!!
-                # print(f'Ask if {i+1} is nearest neighbor with {k + 1}')
-                distance = distance_calculator(object_to_classify, temp_data[k][1:len(temp_data[k])])
-                if distance < nearest_neighbor_distance:
-                    nearest_neighbor_distance = distance
-                    nearest_neighbor_location = k
-                    nearest_neighbor_label = temp_data[nearest_neighbor_location][0]
-        
-        if label_object_to_classify == nearest_neighbor_label:
-            number_correct_classified = number_correct_classified + 1
-
-        #print(f'The {i + 1}th object is in class {label_object_to_classify}')
-        #print(f'Its nearest_neighbor is {nearest_neighbor_location} which is in class {nearest_neighbor_label}')
-    temp_features = list(currentFeatureSet)
-    temp_features.append(featureToAdd)
-
-    accuracy = (number_correct_classified / len(temp_data)) * 100
-
-    print(f'\t\tUsing feature(s) {temp_features} accuracy is {"{:.2f}".format(accuracy)}%')
-
-    trace = open('trace.txt', 'a')
-    trace.write(f'\t\tUsing feature(s) {temp_features} accuracy is {"{:.2f}".format(accuracy)}%\n')
-    trace.close()
-    
-    return accuracy
-
 
 def distance_calculator(origin_instance, next_instance):
     total = 0
