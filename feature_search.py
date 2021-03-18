3605597ddf1bd6ca3f2d28aaac345f687c0d36d2
Renzo <rmolivares@renzo-olivares.dev>
2021-03-17 18:17:42 -0700

    Set precision for floating points to 2 and clean up trace

--- feature_search.py
+++ feature_search.py
@@ -12,6 +12,7 @@ def feature_search(data, selection):
 def forward_search(data):
     all_set_of_features = []
     current_set_of_features = OrderedDict() # Initialize an empty set
+    best_accuracy = None
 
     for i in range(1, len(data[0])):
         print(f'On the {i}th level of the search tree')
@@ -37,27 +38,38 @@ def forward_search(data):
                     best_so_far_accuracy = accuracy
                     feature_to_add_at_this_level = k
         
+        if best_accuracy is not None:
+            if best_so_far_accuracy < best_accuracy:
+                print('\t(Warning, Accuracy has decreased! Continuing search in case of local maxima)')
+                trace = open('trace.txt', 'a')
+                trace.write('\t(Warning, Accuracy has decreased! Continuing search in case of local maxima)\n')
+                trace.close()
+            best_accuracy = best_so_far_accuracy
+        else:
+            best_accuracy = best_so_far_accuracy
+            
         current_set_of_features[i] = feature_to_add_at_this_level
         all_set_of_features.append((best_so_far_accuracy, list(current_set_of_features.values())))
 
-        print(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {best_so_far_accuracy * 100}%')
+        print(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {"{:.2f}".format(best_so_far_accuracy)}%')
         print(f'\tOn level {i} I added feature {feature_to_add_at_this_level} to current set\n')
 
         trace = open('trace.txt', 'a')
-        trace.write(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {best_so_far_accuracy * 100}%\n')
+        trace.write(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {"{:.2f}".format(best_so_far_accuracy)}%\n')
         trace.write(f'\tOn level {i} I added feature {feature_to_add_at_this_level} to current set\n\n')
         trace.close()
 
-    print(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {max(all_set_of_features)[0] * 100}%')
+    print(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {"{:.2f}".format(max(all_set_of_features)[0])}%')
 
     trace = open('trace.txt', 'a')
-    trace.write(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {max(all_set_of_features)[0] * 100}%\n')
+    trace.write(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {"{:.2f}".format(max(all_set_of_features)[0])}%\n')
     trace.close()
 
 def backward_search(data):
     all_set_of_features = []
     current_set_of_features = OrderedDict() # Initialize an empty set
     set_of_features_removed = set()
+    best_accuracy = None
 
     for k in range(1, len(data[0])): # Load current set of features with all features
         current_set_of_features[k] = k
@@ -85,23 +97,33 @@ def backward_search(data):
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
                     feature_to_remove_at_this_level = k
-        
+
+        if best_accuracy is not None:
+            if best_so_far_accuracy < best_accuracy:
+                print('\t(Warning, Accuracy has decreased! Continuing search in case of local maxima)')
+                trace = open('trace.txt', 'a')
+                trace.write('\t(Warning, Accuracy has decreased! Continuing search in case of local maxima)\n')
+                trace.close()
+            best_accuracy = best_so_far_accuracy
+        else:
+            best_accuracy = best_so_far_accuracy
+
         del current_set_of_features[feature_to_remove_at_this_level]
         set_of_features_removed.add(feature_to_remove_at_this_level)
         all_set_of_features.append((best_so_far_accuracy, list(current_set_of_features.values())))
 
-        print(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {best_so_far_accuracy * 100}%')
-        print(f'\tOn level {i} I removed feature {feature_to_remove_at_this_level} to current set\n')
+        print(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {"{:.2f}".format(best_so_far_accuracy)}%')
+        print(f'\tOn level {i} I removed feature {feature_to_remove_at_this_level} from the current set\n')
 
         trace = open('trace.txt', 'a')
-        trace.write(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {best_so_far_accuracy * 100}%\n')
-        trace.write(f'\tOn level {i} I removed feature {feature_to_remove_at_this_level} to current set\n\n')
+        trace.write(f'\tFeature set {list(current_set_of_features.values())} was best, accuracy is {"{:.2f}".format(best_so_far_accuracy)}%\n')
+        trace.write(f'\tOn level {i} I removed feature {feature_to_remove_at_this_level} from the current set\n\n')
         trace.close()
 
-    print(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {max(all_set_of_features)[0] * 100}%')
+    print(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {"{:.2f}".format(max(all_set_of_features)[0])}%')
 
     trace = open('trace.txt', 'a')
-    trace.write(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {max(all_set_of_features)[0] * 100}%\n')
+    trace.write(f'Finished search!! The best feature subset is {max(all_set_of_features)[1]}, which has an accuracy of {"{:.2f}".format(max(all_set_of_features)[0])}%\n')
     trace.close() 
 # Exclude anything not in current feature set and exclude featureToAdd
 
@@ -140,13 +162,16 @@ def leave_one_out_cross_validation_backwards(data, currentFeatureSet, featuresTo
         #print(f'Its nearest_neighbor is {nearest_neighbor_location} which is in class {nearest_neighbor_label}')
     temp_features = currentFeatureSet[:]
     temp_features.remove(featureToRemove)
-    print(f'\t\tUsing feature(s) {temp_features} accuracy is {(number_correct_classified / len(temp_data)) * 100}%')
+
+    accuracy = (number_correct_classified / len(temp_data)) * 100
+
+    print(f'\t\tUsing feature(s) {temp_features} accuracy is {"{:.2f}".format(accuracy)}%')
 
     trace = open('trace.txt', 'a')
-    trace.write(f'\t\tUsing feature(s) {temp_features} accuracy is {(number_correct_classified / len(temp_data)) * 100}%\n')
+    trace.write(f'\t\tUsing feature(s) {temp_features} accuracy is {"{:.2f}".format(accuracy)}%\n')
     trace.close()
     
-    return number_correct_classified / len(temp_data)
+    return accuracy
 
 def leave_one_out_cross_validation_forward(data, currentFeatureSet, featureToAdd):
     number_correct_classified = 0
@@ -183,13 +208,16 @@ def leave_one_out_cross_validation_forward(data, currentFeatureSet, featureToAdd
         #print(f'Its nearest_neighbor is {nearest_neighbor_location} which is in class {nearest_neighbor_label}')
     temp_features = list(currentFeatureSet)
     temp_features.append(featureToAdd)
-    print(f'\t\tUsing feature(s) {temp_features} accuracy is {(number_correct_classified / len(temp_data)) * 100}%')
+
+    accuracy = (number_correct_classified / len(temp_data)) * 100
+
+    print(f'\t\tUsing feature(s) {temp_features} accuracy is {"{:.2f}".format(accuracy)}%')
 
     trace = open('trace.txt', 'a')
-    trace.write(f'\t\tUsing feature(s) {temp_features} accuracy is {(number_correct_classified / len(temp_data)) * 100}%\n')
+    trace.write(f'\t\tUsing feature(s) {temp_features} accuracy is {"{:.2f}".format(accuracy)}%\n')
     trace.close()
     
-    return number_correct_classified / len(temp_data)
+    return accuracy
 
 
 def distance_calculator(origin_instance, next_instance):
